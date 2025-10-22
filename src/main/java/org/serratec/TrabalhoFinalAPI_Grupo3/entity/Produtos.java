d4c211361849b48d91e71f2aa631ed3c9731c04a
“Kayo <kayo.lrc@gmail.com>
2025-10-22 20:28:18 -0300

    primeira adicao

--- src/main/java/org/serratec/TrabalhoFinalAPI_Grupo3/entity/Produtos.java
+++ src/main/java/org/serratec/TrabalhoFinalAPI_Grupo3/entity/Produtos.java
@@ -1,13 +1,37 @@
 package org.serratec.TrabalhoFinalAPI_Grupo3.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="produtos")
 public class Produtos {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produto")
 	private Long Id;
 	
+	@NotBlank
+	@Size(min=3, max=100, message="Nome deve ter entre: 3 e 100 Caracteres.")
+	@Column(name="nome", nullable=false, length=100)
 	private String nome;
 	
+	
+	@NotBlank
+	@Size(min=3, max=50, message="Categoria deve ter entre: 3 e 50 Caracteres.")
+	@Column(name="categoria", nullable=false, length=50)
 	private String categoria;
 	
+	@NotNull
+	@Column(name="preco", nullable=false)
 	private Double preco;
 
 	public Produtos(Long id, String nome, String categoria, Double preco) {
