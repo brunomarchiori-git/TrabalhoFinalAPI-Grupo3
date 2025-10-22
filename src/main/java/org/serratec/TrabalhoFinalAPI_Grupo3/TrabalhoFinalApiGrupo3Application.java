2bd693e5d31f5b15e6e88e0e7a3b7d955fc71436
brunomarchiori-git <bruno.marchiori.git@gmail.com>
2025-10-22 18:48:24 -0300

    first commit

---
+++ src/main/java/org/serratec/TrabalhoFinalAPI_Grupo3/TrabalhoFinalApiGrupo3Application.java
@@ -0,0 +1,13 @@
+package org.serratec.TrabalhoFinalAPI_Grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalApiGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalApiGrupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/TrabalhoFinalAPI_Grupo3/TrabalhoFinalApiGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.TrabalhoFinalAPI_Grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApiGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
