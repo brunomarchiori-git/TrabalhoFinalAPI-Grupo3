147df0007274cbecafe1dbc01205209e2d80e966
jessicamaglianoti <jessicamagliano.ti@gmail.com>
2025-10-22 22:10:07 -0300

    Implementei a entity Categorias

--- src/main/java/org/serratec/TrabalhoFinalAPI_Grupo3/entity/Categorias.java
+++ src/main/java/org/serratec/TrabalhoFinalAPI_Grupo3/entity/Categorias.java
@@ -1,10 +1,30 @@
 package org.serratec.TrabalhoFinalAPI_Grupo3.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categorias")
 public class Categorias {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_categoria")
 	private Long Id;
-	
+
+	@NotBlank(message = "O nome da categoria não pode ser vazio.")
+	@Size(min = 2, max = 50, message = "Nome da categoria deve ter entre 2 e 50 Caracteres.")
+	@Column(name = "nome", nullable = false, length = 50)
 	private String nome;
+
+	public Categorias() {
+	}
 
 	public Categorias(Long id, String nome) {
 		super();
@@ -12,9 +32,7 @@ public class Categorias {
 		this.nome = nome;
 	}
 
-	public Categorias() {
-	}
-
+	// Getters e Setters
 	public Long getId() {
 		return Id;
 	}
@@ -30,8 +48,4 @@ public class Categorias {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
-	
-	
 }
